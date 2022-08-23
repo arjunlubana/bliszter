edf7dbd3f3ac0e1e02eb656374ee1f18e4ccb55a
Arjun Lubana <arjunlubana562@gmail.com>
2022-08-23 18:44:10 +0300

    Save devto integration tokens

--- utils/index.ts
+++ utils/index.ts
@@ -2,4 +2,7 @@ import axios from "axios";
 
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
-export { fetcher };
+const poster = (url: string, data: Object) =>
+  axios.post(url, data).then((res) => res.data);
+
+export { fetcher, poster };
