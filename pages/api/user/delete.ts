3c402d20f7a8edc9c1a372b5c6271022c79bd1ee
Arjun Lubana <arjunlubana562@gmail.com>
2022-08-23 13:51:51 +0300

    Revoke tokens when deleting users

--- pages/api/user/delete.ts
+++ pages/api/user/delete.ts
@@ -2,6 +2,7 @@ import { superTokensNextWrapper } from "supertokens-node/nextjs";
 import { verifySession } from "supertokens-node/recipe/session/framework/express";
 import { deleteUser } from "supertokens-node";
 import { SessionRequest } from "supertokens-node/framework/express";
+import Session from "supertokens-node/recipe/session";
 
 type Data = { name: string };
 export default async function delete_user(req: SessionRequest, res: any) {
@@ -14,8 +15,10 @@ export default async function delete_user(req: SessionRequest, res: any) {
     res
   );
   // if it comes here, it means that the session verification was successful
-;
-  await deleteUser(req.session!.getUserId());
-  await req.session!.revokeSession();
-  res.send("Success! User session revoked");
+  // if session is valid we revoke all sessions then delete user
+  let userId = req.session!.getUserId();
+  
+  await Session.revokeAllSessionsForUser(userId);
+  await deleteUser(userId);
+  res.status(200).send("Success! User session revoked");
 }
