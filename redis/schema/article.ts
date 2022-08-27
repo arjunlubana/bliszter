1c203fca39b23b36a61a11c88ae51d7e17c2a37a
Arjun Lubana <arjunlubana562@gmail.com>
2022-08-27 05:43:17 +0300

    Update the article schema in redis

--- redis/schema/article.ts
+++ redis/schema/article.ts
@@ -5,22 +5,33 @@ import redis from "../redis";
 class Article extends Entity {}
 interface Article {
   userId: string;
+  title: string;
+  markdown: string;
   medium_url: string;
   hashnode_url: string;
   devto_url: string;
 }
 /* create a Schema for Person */
 const articleSchema = new Schema(Article, {
-  make: { type: "string" },
-  model: { type: "string" },
-  image: { type: "string" },
-  description: { type: "string" },
+  userId: { type: "string" },
+  title: { type: "string" },
+  markdown: { type: "string" },
+  medium_url: { type: "string" },
+  hashnode_url: { type: "string" },
+  devto_url: { type: "string" },
 });
 
 /* use the redis client to create a Repository just for Articles */
-export const articleRepository = redis.fetchRepository(articleSchema);
+const articleRepository = redis.fetchRepository(articleSchema);
 
 /* create the index for Person */
 (async () => {
   await articleRepository.createIndex();
 })();
+
+async function createArticle(article) {
+  const id = await articleRepository.createAndSave(article);
+  return id;
+}
+
+export { createArticle };
