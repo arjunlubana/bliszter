90113cd1859360fb8c659b53e1d33eab04436344
Arjun Lubana <arjunlubana562@gmail.com>
2022-08-28 10:53:35 +0300

    Add two columns to editor layout

--- layouts/editor-layout.tsx
+++ layouts/editor-layout.tsx
@@ -1,3 +1,12 @@
+import { Grid, GridItem } from "@chakra-ui/react";
+
 export default function EditorLayout({ children }) {
-  return <>{children}</>;
+  return (
+    <Grid h="100%" templateColumns="repeat(6, 1fr)" gap={4}>
+      <GridItem colSpan={1} bg="tomato" />
+      <GridItem colStart={2} colEnd={7} bg="papayawhip">
+        {children}
+      </GridItem>
+    </Grid>
+  );
 }
