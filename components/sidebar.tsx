5e16422b4b65c73751abcffb610e3d234a1d161b
Arjun Lubana <arjunlubana562@gmail.com>
2022-08-28 12:57:11 +0300

    Add links to sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,27 +1,27 @@
-import { EditIcon, SearchIcon, SettingsIcon } from "@chakra-ui/icons";
-import { Avatar, Flex, IconButton, Spacer } from "@chakra-ui/react";
+import { EditIcon, SettingsIcon, StarIcon } from "@chakra-ui/icons";
+import { Avatar, Flex, IconButton, Link, Spacer } from "@chakra-ui/react";
+import NextLink from "next/link";
 
 export default function Sidebar() {
   return (
     <Flex h="100%" direction="column" py="1em" align="center">
-      <IconButton
-        aria-label="Write an article"
-        icon={<EditIcon />}
-        size="lg"
-        fontSize="24px"
-      />
-      <IconButton
-        aria-label="Settings"
-        icon={<SettingsIcon />}
-        size="lg"
-        fontSize="24px"
-      />
+      <NextLink href="/dashboard" passHref>
+        <Link>
+          <StarIcon boxSize="1.5em" m="1em" />
+        </Link>
+      </NextLink>
+      <NextLink href="/dashboard/editor" passHref>
+        <Link>
+          <EditIcon boxSize="1.5em" m="1em" />
+        </Link>
+      </NextLink>
+      <NextLink href="/dashboard/integrations" passHref>
+        <Link>
+          <SettingsIcon boxSize="1.5em" m="1em" />
+        </Link>
+      </NextLink>
       <Spacer />
-      <Avatar
-        size="sm"
-        name="Kent Dodds"
-        src="https://bit.ly/kent-c-dodds"
-      />
+      <Avatar size="sm" name="Kent Dodds" src="https://bit.ly/kent-c-dodds" />
     </Flex>
   );
 }
