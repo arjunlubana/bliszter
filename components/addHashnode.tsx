13cfdf4dccc22dee75fa71417a446fc975c9c58c
Arjun Lubana <arjunlubana562@gmail.com>
2022-08-28 17:44:15 +0300

    Save hashnode publication id to supertokens user metadata

--- components/addHashnode.tsx
+++ components/addHashnode.tsx
@@ -1,28 +1,41 @@
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import { Button, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { FormEvent } from "react";
 import { poster } from "../utils";
 
 export default function AddHashnode() {
+  const [username, setUsername] = useState("");
+  const [hashnodeToken, setHashnodeToken] = useState("");
   const [show, setShow] = React.useState(false);
   const handleClick = () => setShow(!show);
-  
-  const onIntegration = async (e: FormEvent<HTMLFormElement>) => {
+
+  const onIntegration = async (e) => {
     e.preventDefault();
-    const form = new FormData(e.target);
-    const formData = Object.fromEntries(form.entries());
-    console.log(formData)
-    await poster("/api/hashnode/integrate", formData);
+    let data = {
+      username,
+      hashnodeToken,
+    };
+    await poster("/api/hashnode/integrate", data);
   };
   return (
     <form onSubmit={onIntegration}>
-      <label htmlFor="Dev To Integration Token">Integration Token</label>
+      <Input
+        pr="4.5rem"
+        type={"text"}
+        placeholder="Your Username"
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+      />
       <InputGroup size="md">
         <Input
           pr="4.5rem"
           type={show ? "text" : "password"}
           placeholder="Your Integration Token"
+          onChange={(e) => {
+            setHashnodeToken(e.target.value);
+          }}
         />
         <InputRightElement width="3rem">
           <Button h="1.75rem" size="sm" onClick={handleClick}>
--- pages/api/hashnode/integrate.ts
+++ pages/api/hashnode/integrate.ts
@@ -1,3 +1,4 @@
+import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
 import { SessionRequest } from "supertokens-node/framework/express";
 import UserMetadata from "supertokens-node/recipe/usermetadata";
 import authorize from "../../../supertokens/authorize";
@@ -6,10 +7,41 @@ export default async function integrate(req: SessionRequest, res: any) {
   await authorize(req, res);
   const session = (req as SessionRequest).session;
   const userId = session!.getUserId();
-  const hashnode_token = req.body.hashnode_token;
-
-  await UserMetadata.updateUserMetadata(userId, {
-    hashnode_token: hashnode_token
+  const hashnode_token = req.body.hashnodeToken;
+  const client = new ApolloClient({
+    uri: "https://api.hashnode.com/",
+    cache: new InMemoryCache(),
   });
-  res.json({ message: "successfully integration with hashnode" });
+  try {
+    const response = await client.query({
+      query: gql`
+        query GET_PUBLICATION_ID($username: String!) {
+          user(username: $username) {
+            publication {
+              _id
+            }
+          }
+        }
+      `,
+      variables: {
+        username: req.body.username,
+      },
+    });
+
+    await UserMetadata.updateUserMetadata(userId, {
+      hashnode_token: hashnode_token,
+      hashnode_publication: response.data.user.publication._id,
+    });
+
+    res.json({
+      title: "Integrated with Hashnode",
+      status: "success",
+    });
+  } catch (error) {
+    res.json({
+      title: "Failed to Integrate with Hashnode",
+      description: "We are unable to integrate with hashnode",
+      status: "error",
+    });
+  }
 }
