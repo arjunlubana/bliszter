75a936758ed6656a74039675c57bcf065a2e0b93
Arjun Lubana <arjunlubana562@gmail.com>
2022-08-30 13:18:27 +0300

    Update frontend config

--- supertokens/frontendConfig.ts
+++ supertokens/frontendConfig.ts
@@ -1,32 +1,19 @@
-import ThirdPartyEmailPasswordReact from "supertokens-auth-react/recipe/thirdpartyemailpassword";
-import SessionReact from "supertokens-auth-react/recipe/session";
+import ThirdPartyEmailPasswordReact from 'supertokens-auth-react/recipe/thirdpartyemailpassword'
+import SessionReact from 'supertokens-auth-react/recipe/session'
+import { appInfo } from './appInfo'
+import Router from 'next/router'
 
-export function getApiDomain() {
-  if (window.location.hostname === "localhost") {
-    return "http://localhost:3000";
-  }
-  return window.location.origin;
-}
-
-export function getWebsiteDomain() {
-  return window.location.origin;
-}
-
-export const frontendConfig = () => {
+export let frontendConfig = () => {
   return {
-    appInfo: {
-      appName: "Bliszter", // TODO: Your app name
-      apiDomain: getApiDomain(), // TODO: Change to your app's API domain
-      websiteDomain: getWebsiteDomain(), // TODO: Change to your app's website domain
-      apiBasePath: "/api/auth/",
-      websiteBasePath: "/auth",
-    },
+    appInfo,
     recipeList: [
       ThirdPartyEmailPasswordReact.init({
+        emailVerificationFeature: {
+          mode: 'REQUIRED',
+        },
         signInAndUpFeature: {
           providers: [
             ThirdPartyEmailPasswordReact.Google.init(),
-            ThirdPartyEmailPasswordReact.Facebook.init(),
             ThirdPartyEmailPasswordReact.Github.init(),
             ThirdPartyEmailPasswordReact.Apple.init(),
           ],
@@ -34,5 +21,17 @@ export const frontendConfig = () => {
       }),
       SessionReact.init(),
     ],
-  };
-};
+    // this is so that the SDK uses the next router for navigation
+    windowHandler: (oI: any) => {
+      return {
+        ...oI,
+        location: {
+          ...oI.location,
+          setHref: (href: any) => {
+            Router.push(href)
+          },
+        },
+      }
+    },
+  }
+}
