5c0393dc6e8e89b69aae81ea8b76e2ceb616324e
Arjun Lubana <arjunlubana562@gmail.com>
2022-08-30 13:46:31 +0300

    Update the appInfo

--- supertokens/appInfo.ts
+++ supertokens/appInfo.ts
@@ -1,6 +1,9 @@
+const port = process.env.APP_PORT || 3000
+
 const apiBasePath = '/api/auth/'
 
 export const websiteDomain = process.env.VERCEL_URL || window.location.origin
+console.log(websiteDomain)
 
 export const appInfo = {
   appName: 'Bliszter',
