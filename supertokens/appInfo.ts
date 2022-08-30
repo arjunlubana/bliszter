9f1d20cacae692edd7754c8bc6f4da92bd575ba8
Arjun Lubana <arjunlubana562@gmail.com>
2022-08-30 22:17:16 +0300

    A few changes that might fix errors

--- supertokens/appInfo.ts
+++ supertokens/appInfo.ts
@@ -1,19 +1,11 @@
 const port = process.env.APP_PORT || 3000
 const apiBasePath = '/api/auth/'
 
-
-let websiteDomain =
-  process.env.APP_URL ||
-  process.env.NEXT_PUBLIC_APP_URL ||
-  process.env.VERCEL_URL
-
-if (process.env.NODE_ENV !== "production") {
-  websiteDomain =
+const websiteDomain =
     process.env.APP_URL ||
     process.env.NEXT_PUBLIC_APP_URL ||
     process.env.VERCEL_URL ||
     `http://localhost:${port}`
-}
 
 const appInfo = {
   appName: 'SuperTokens Demo App',
