5578d03ffca095b8e4812f928580fc03477c5921
Arjun Lubana <arjunlubana562@gmail.com>
2022-08-30 13:06:56 +0300

    Update the backend Config

--- pages/api/auth/[[...path]].ts
+++ pages/api/auth/[[...path]].ts
@@ -1,26 +1,45 @@
-import { superTokensNextWrapper } from 'supertokens-node/nextjs'
-import { middleware } from 'supertokens-node/framework/express'
+// import { superTokensNextWrapper } from 'supertokens-node/nextjs'
+// import { middleware } from 'supertokens-node/framework/express'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { Request, Response } from 'express';
+// import supertokens from 'supertokens-node'
+// import { backendConfig } from '../../../supertokens/backendConfig'
+
+// supertokens.init(backendConfig())
+
+// export default async function superTokens(
+//   req: NextApiRequest & Request,
+//   res: NextApiResponse & Response
+// ) {
+
+//   // // NOTE: We need CORS only if we are querying the APIs from a different origin
+//   // await NextCors(req, res, {
+//   //   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+//   //   origin: apiDomain,
+//   //   credentials: true,
+//   //   allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
+//   // });
+
+//   await superTokensNextWrapper(
+//     async (next) => {
+//       await middleware()(req, res, next)
+//     },
+//     req,
+//     res
+//   )
+//   if (!res.writableEnded) {
+//     res.status(404).send('Not found')
+//   }
+// }
+
+import { superTokensNextWrapper } from 'supertokens-node/nextjs'
 import supertokens from 'supertokens-node'
+import { middleware } from 'supertokens-node/framework/express'
 import { backendConfig } from '../../../supertokens/backendConfig'
-import NextCors from "nextjs-cors";
 
 supertokens.init(backendConfig())
 
-export default async function superTokens(
-  req: NextApiRequest & Request,
-  res: NextApiResponse & Response
-) {
-
-  // // NOTE: We need CORS only if we are querying the APIs from a different origin
-  // await NextCors(req, res, {
-  //   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-  //   origin: apiDomain,
-  //   credentials: true,
-  //   allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
-  // });
-
+export default async function superTokens(req: NextApiRequest & Request, res: NextApiResponse & Response) {
   await superTokensNextWrapper(
     async (next) => {
       await middleware()(req, res, next)
